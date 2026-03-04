be5298ae95e460d703a80e4d34956e0dc27d76de
alex <343072005@tdsb.ca>
2026-03-04 09:54:17 -0500

    adde document get id and updated html values, broken

--- main.ts
+++ main.ts
@@ -2,19 +2,40 @@ const form = document.getElementById("cubic-form") as HTMLFormElement;
 
 let roots: number[] = [];
 
-function trignometricMethod(a: number, b: number, p: number, q: number) {
-    const theta: number = (1 / 3) * Math.acos(-q / (2 * Math.sqrt(-Math.pow(p / 3, 3))))
-    const y1: number = 2 * Math.sqrt(-p / 3) * Math.cos(theta)
-    const y2: number = 2 * Math.sqrt(-p / 3) * Math.cos(theta + (2 * Math.PI) / 3)
-    const y3: number = 2 * Math.sqrt(-p / 3) * Math.cos(theta + (4 * Math.PI) / 3)
-    roots = [y1, y2, y3]
-    roots = roots.map((root) => root - b / (3 * a))
+function trignometricMethod(a: number, b: number, p: number, q: number): void {
+    const theta: number = (1 / 3) * Math.acos(-q / (2 * Math.sqrt(-Math.pow(p / 3, 3))));
+    const y1: number = 2 * Math.sqrt(-p / 3) * Math.cos(theta);
+    const y2: number = 2 * Math.sqrt(-p / 3) * Math.cos(theta + (2 * Math.PI) / 3);
+    const y3: number = 2 * Math.sqrt(-p / 3) * Math.cos(theta + (4 * Math.PI) / 3);
+    roots = [y1, y2, y3];
+    roots = roots.map((root) => root - b / (3 * a));
 };
 
-const cardanosMethod = (a: number, b: number, q: number, disciminant: number) => {
+const cardanosMethod = (a: number, b: number, q: number, disciminant: number): number => {
     return (
         Math.cbrt(-q / 2 + Math.sqrt(disciminant)) + Math.cbrt(-q / 2 - Math.sqrt(disciminant)) - b / (3 * a)
     );
+};
+
+function displayResults (p: number, q: number, discriminant: number) {
+    (document.getElementById("p") as HTMLInputElement).value = `${p}`;
+    (document.getElementById("p") as HTMLInputElement).value = `${q}`;
+    (document.getElementById("discriminant") as HTMLInputElement).value = `${discriminant}`;
+    if (roots.length === 3) {
+        (document.getElementById("root-one") as HTMLInputElement).value = `${roots[0]}`;
+        (document.getElementById("root-two") as HTMLInputElement).value = `${roots[1]}`;
+        (document.getElementById("root-three") as HTMLInputElement).value = `${roots[2]}`;
+    } else {
+        if (discriminant > 0) {
+            (document.getElementById("root-one") as HTMLInputElement).value = `${roots[0]}`;
+            (document.getElementById("root-two") as HTMLInputElement).value = "complex";
+            (document.getElementById("root-three") as HTMLInputElement).value = "complex";
+        } else {
+            (document.getElementById("root-one") as HTMLInputElement).value = `${roots[0]}`;
+            (document.getElementById("root-two") as HTMLInputElement).value = `${roots[0]}`;
+            (document.getElementById("root-three") as HTMLInputElement).value = `${roots[0]}`;
+        };
+    };
 };
 
 form?.addEventListener("submit", (event) => {
@@ -32,23 +53,22 @@ form?.addEventListener("submit", (event) => {
 
     const discriminant = Math.pow(q / 2, 2) + Math.pow(p / 3, 3);
 
-    console.log(a, b, c, d)
-
     if (discriminant < 0) {
-        trignometricMethod(a, b, p, q)
+        trignometricMethod(a, b, p, q);
     } else if (discriminant > 0) {
-        roots = [cardanosMethod(a, b, q, discriminant)]
+        roots = [cardanosMethod(a, b, q, discriminant)];
     } else {
         if (p === 0 && q === 0) {
-            roots = [cardanosMethod(a, b, q, discriminant)]
+            roots = [cardanosMethod(a, b, q, discriminant)];
         } else {
             roots = [
                 cardanosMethod(a, b, q, discriminant),
-                // cardanosMethod(a, b, q, discriminant), // when i do 2 cardanos, i get the wrong double root
                 Math.cbrt(q / 2) - b / (3 * a),
                 Math.cbrt(q / 2) - b / (3 * a)
-            ]
-        }
-    }
-    console.log(roots)
+            ];
+        };
+    };
+    console.log(a, b, c, d);
+    console.log(roots);
+    displayResults(p, q, discriminant);
 });
